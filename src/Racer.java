0e4b6c211b4d8569eac18915804d2fc13aa6f399
laurennelson <laurenm.nelson@calbaptist.edu>
2021-01-29 17:38:52 -0800

    removed unused imports

--- src/Racer.java
+++ src/Racer.java
@@ -1,8 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.util.*;
-import java.time.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
